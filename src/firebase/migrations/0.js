0b49a6220bfbf4998b2639736c34c26482076b51
Juan M <juan.martinez@districtm.net>
2019-12-23 12:04:27 -0500

    Add Christmas combo

--- src/firebase/migrations/0.js
+++ src/firebase/migrations/0.js
@@ -4,14 +4,54 @@ const up = () => {
   let order = 1;
 
   const sections = [
-    { order: order++, name: "rolls", color: colors.black },
-    { order: order++, name: "bowls", color: colors.golden },
-    { order: order++, name: "pokes", color: colors.red },
-    { order: order++, name: "combos", color: colors.golden },
-    { order: order++, name: "menú infaltil : )", color: colors.green },
-    { order: order++, name: "baos", color: colors.red },
-    { order: order++, name: "spring rolls vegetarianos", color: colors.golden },
-    { order: order++, name: "bebidas", color: colors.black }
+    {
+      order: order++,
+      name: "rolls",
+      menuName: "rolls",
+      color: colors.black
+    },
+    {
+      order: order++,
+      name: "combos",
+      menuName: "combos",
+      color: colors.golden
+    },
+    {
+      order: order++,
+      name: "bowls",
+      menuName: "bowls",
+      color: colors.golden
+    },
+    {
+      order: order++,
+      name: "pokes",
+      menuName: "pokes",
+      color: colors.red
+    },
+    {
+      order: order++,
+      name: "menú infaltil : )",
+      menuName: "niños",
+      color: colors.green
+    },
+    {
+      order: order++,
+      name: "baos",
+      menuName: "baos",
+      color: colors.red
+    },
+    {
+      order: order++,
+      name: "spring rolls vegetarianos",
+      menuName: "vegetarianos",
+      color: colors.golden
+    },
+    {
+      order: order++,
+      name: "bebidas",
+      menuName: "bebidas",
+      color: colors.black
+    }
   ];
 
   sections.map(section => {
